7723166c314f0866cf04eeeaadc5146e5d84708e
cristianscript <chris191919torres@gmail.com>
2023-10-28 13:37:35 -0500

    feat: added foundational unit test to avoid future production bugs to users

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  cacheDirectory: '.tmp/jestCache'
-};
+  cacheDirectory: '.tmp/jestCache',
+  testPathIgnorePatterns: [
+    'node_modules',
+    'dist'
+  ],
+  collectCoverage: true,
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios')
+  }
+}
